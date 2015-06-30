ca1666442b0fe36ec6c10d46e6a594bd60d85a62
Anastasia <anastasia.v.sokolova@gmail.com>
2015-06-30 15:34:22 +0500

    Добавлены формы для выбора дат на страницы Таблица посещаемости, Производственный план, Отчет по дизайнерам. Исправлена ошибка в формировании списка дат в prod_plan_view

--- asuzr/forms.py
+++ asuzr/forms.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from datetime import date
+from django.contrib.admin.widgets import AdminDateWidget
 
 class DateForm (forms.Form):
-  date = forms.DateField(label = 'Date')
+  date = forms.DateField(widget = AdminDateWidget, label = u'Дата', initial = date.today)
+  
+class DiapDateForm (forms.Form):
+  sdate = forms.DateField(widget = AdminDateWidget, label = u'С', initial = date.today)
+  edate = forms.DateField(widget = AdminDateWidget, label = u'по', initial = date.today)
