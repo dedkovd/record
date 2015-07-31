fc876e1c5c08916d6228e078d5878167ff1115ba
Denis V. Dedkov <denis.v.dedkov@gmail.com>
2015-07-31 23:22:57 +0500

    Логирование
    Добавил декоратор логирования для вьюх

--- asuzr/common.py
+++ asuzr/common.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*- 
 from datetime import date
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.models import User
 
 class custom_date(date):
 
@@ -21,3 +23,19 @@ class custom_date(date):
  @property
  def is_weekend(self):
     return self.weekday() >= 5
+
+
+def log_view_call(fn):
+    '''
+    Wrapper for views log
+    '''
+    def wrapper(*args, **kwargs):
+        log_entry = {}
+        request = args[0]
+        log_entry['user'] = User.objects.get(username = request.META['USER'])
+        log_entry['object_repr'] = fn.__name__
+        log_entry['action_flag'] = 5
+        entry = LogEntry(**log_entry)
+        entry.save()
+        return fn(*args, **kwargs)
+    return wrapper
